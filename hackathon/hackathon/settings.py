d486b4d08a37daf5e6b6384c42febc80281a41e4
SashaGood <test@mail.ru>
2024-11-28 13:51:13 +0300

    bug fix

--- hackathon/hackathon/settings.py
+++ hackathon/hackathon/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authorization',
-    'parsers'
-    'data',
+    'parsers',
+    'data'
 ]
 
 MIDDLEWARE = [
