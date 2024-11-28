1a7942f6fcdc0658cc233a0c7c5594bc9ea9881d
SashaGood <test@mail.ru>
2024-11-28 13:59:36 +0300

    parser update

--- hackathon/parsers/parser.py
+++ hackathon/parsers/parser.py
@@ -1,31 +1,14 @@
-import csv
-from pathlib import Path
-
+import pandas as pd
 from django.conf import settings
-import requests
+
+from data.models import Client
 
 
 class Parser:
-    def __init__(self, clients_url=None, address_url=None, package_channel_url=None, activities_url=None):
-        clients_url = clients_url if clients_url else settings.CLIENTS_URL
-
-        response = requests.get(clients_url)
-        #self.address = requests.get(address_url if address_url else settings.ADDRESS_URL)
-        #self.package_channel = requests.get(
-        #    package_channel_url if package_channel_url else settings.PACKAGE_CHANNEL_URL)
-        #self.activities = requests.get(activities_url if activities_url else settings.ACTIVITIES_URL)
-        if response.status_code == 200:
-            decoded_content = response.content.decode('utf-8-sig')
-            rows = [row.split(',') for row in decoded_content.splitlines()]
-
-            output_path = Path(settings.FILES_DIR) / 'clients.csv'
-            with open(output_path, mode='w', newline='', encoding='utf-8-sig') as file:
-                writer = csv.writer(file)
-                writer.writerows(rows)
-        else:
-            print(f"Ошибка при загрузке файла: {response.status_code}")
-
-    def init_data(self):
-        123
     def fill(self):
-        123
+        df = pd.read_csv(settings.FILES_DIR / 'client.csv')
+        clients = []
+        for row in df.iterrows():
+            data1 = row[1][0].split(';')
+            data2 = row[0].split(';')
+            clients.append(Client(external_id=data2[0],gender=data1[1],age_min=data1[2].split('-')[0],age_max=data1[2].split('-')[1],address=data2[1] + data1[0]))
