a7cae04a809735e865528485ce130e7c4a99c644
SashaGood <test@mail.ru>
2024-11-28 14:54:20 +0300

    test url

--- hackathon/data/views.py
+++ hackathon/data/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from data.models import Client
+
 
 # Create your views here.
+class TestView(generics.ListAPIView):
+    queryset = Client.objects.all()
