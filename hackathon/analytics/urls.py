c11a06e64368f17a2d54654c39e0400316c99537
Kotov <Kotov.Sasho204@yandex.ru>
2024-11-29 14:56:26 +0300

    fix

--- hackathon/analytics/urls.py
+++ hackathon/analytics/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from analytics.views import MostViewedTWShowsView, MostViewedChannelsView
+from analytics.views import MostViewedTWShowsView
 
 urlpatterns = [
     path('most_viewed_tw_shows/', MostViewedTWShowsView.as_view()),
-    path('most_viewed_channels/', MostViewedChannelsView.as_view()),
 ]
