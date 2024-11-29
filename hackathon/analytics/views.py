b415e3fa749cc9ded2a75e44443b6b50d7f2db46
Kotov <Kotov.Sasho204@yandex.ru>
2024-11-29 15:20:24 +0300

    rename

--- hackathon/analytics/views.py
+++ hackathon/analytics/views.py
@@ -21,24 +21,23 @@ class TVShowSerializer(serializers.ModelSerializer):
 
 
 class ResponseSerializer(serializers.Serializer):
-    tw_shows = TVShowSerializer(many=True)
+    tv_shows = TVShowSerializer(many=True)
 
 
-class MostViewedTWShowsView(APIView):
+class MostViewedTVShowsView(APIView):
 
     def post(self, request, *args, **kwargs):
         serializer = RequestSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         data = serializer.data
-        # viewing = Viewing.objects.all()
-        tw_shows = TVShow.objects.all()
+        tv_shows = TVShow.objects.all()
         q = Q()
         if data.get('start_time'):
             q = q & Q(viewing__start_time__gte=data.get('start_time'))
         if data.get('finish_time'):
             q = q & Q(viewing__finish_time__lte=data.get('finish_time'))
 
-        tw_shows = tw_shows.annotate(view_count=Count('viewing', filter=q))
-        tw_shows = tw_shows.order_by('-view_count')
+        tv_shows = tv_shows.annotate(view_count=Count('viewing', filter=q))
+        tv_shows = tv_shows.order_by('-view_count')
 
-        return Response(ResponseSerializer({'tw_shows': tw_shows[:50]}).data, status=status.HTTP_200_OK)
+        return Response(ResponseSerializer({'tv_shows': tv_shows[:50]}).data, status=status.HTTP_200_OK)
