ed96289109a83c0d3a1cc468c955ede4942f2c43
Kotov <Kotov.Sasho204@yandex.ru>
2024-11-30 10:47:48 +0300

    no env

--- hackathon/hackathon/settings.py
+++ hackathon/hackathon/settings.py
@@ -156,12 +156,11 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 465
-DEFAULT_FROM_EMAIL = os.getenv('EMAIL')
-EMAIL_HOST_USER = os.getenv('EMAIL')
-EMAIL_HOST_PASSWORD = os.getenv('PASSWORD')
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = 'fhkvyuhv.777@yandex.ru'
+EMAIL_HOST_USER = 'fhkvyuhv.777@yandex.ru'
+EMAIL_HOST_PASSWORD = 'hhrnbttycxrzhmbg'
+EMAIL_USE_TLS = True
 
 CLIENTS_URL = os.getenv('CLIENTS_URL')
 ADDRESS_URL = os.getenv('ADDRESS_URL')
